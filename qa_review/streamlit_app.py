dafaedc6b0a469ba73a67bb77c7228acd624676b
rayenbbs <rayenbesbesbesbes@gmail.com>
2024-12-17 14:12:57 +0100

    test

--- qa_review/streamlit_app.py
+++ qa_review/streamlit_app.py
@@ -9,7 +9,7 @@ from src.qa_review.crew import QaReview
 from dotenv import load_dotenv
 from UI.utilities.helpers import is_valid_url, load_markdown, display_charts,display_download_buttons
 import path
-dir = path.Path(__file__).abspath()
+dir = path.Path(__file__)
 sys.append.path(dir.parent.parent)
 load_dotenv()
 
