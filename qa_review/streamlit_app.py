936b2ca6a9ed11922fb60624b00e080c796db281
rayenbbs <rayenbesbesbesbes@gmail.com>
2024-12-17 01:13:45 +0100

    added pysqlite3 to streamlitapp

--- qa_review/streamlit_app.py
+++ qa_review/streamlit_app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from src.qa_review.crew import QaReview
 from dotenv import load_dotenv
